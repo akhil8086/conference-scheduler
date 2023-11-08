fea4836d5b07d2fcb0866a3ed44c39852ac62bc2
akhil8086 <akhiljith.r@edstem.com>
2023-11-08 23:52:33 +0530

    final commit

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -69,51 +69,53 @@ function ConferenceSchedule() {
 
   return (
     <div>
-      <Header />
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold mb-4">Conference Schedule</h1>
+    <Header />
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">Conference Schedule</h1>
 
-        <form onSubmit={handleSubmit} className="mb-4">
-          <div className="flex flex-col mb-4 w-64">
-            <input
-              type="text"
-              name="talk"
-              value={formData.talk}
-              onChange={handleChange}
-              placeholder="Talk"
-              className="border p-2 rounded"
-            />
-          </div>
-          <div className="flex flex-col mb-4 w-64">
-            <input
-              type="text"
-              name="time"
-              value={formData.time}
-              onChange={handleChange}
-              placeholder="Time"
-              className="border p-2 rounded"
-            />
-          </div>
-          {isFormEnabled ? (
-            <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-              Add Schedule
-            </button>
-          ) : null}
-        </form>
+      <form onSubmit={handleSubmit} className="mb-4 flex flex-col items-center">
+  <div className="flex flex-col mb-4 w-64">
+    <input
+      type="text"
+      name="talk"
+      value={formData.talk}
+      onChange={handleChange}
+      placeholder="Talk"
+      className="border p-2 rounded"
+    />
+  </div>
+  <div className="flex flex-col mb-4 w-64">
+    <input
+      type="text"
+      name="time"
+      value={formData.time}
+      onChange={handleChange}
+      placeholder="Time"
+      className="border p-2 rounded"
+    />
+  </div>
+  {isFormEnabled ? (
+    <button type="submit" className="bg-blue-500 text-white p-2 rounded ml-2">
+      Add Schedule
+    </button>
+  ) : null}
+</form>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {scheduleData.map((item, index) => (
-            <div key={index} className="bg-blue-200 border p-4 rounded">
-              {editingIndex === index ? (
-                <div>
-                  <button onClick={handleSaveEdit} className="bg-green-500 text-white p-2 rounded">
-                    Save
-                  </button>
-                </div>
-              ) : (
-                <div>
-                  <h2 className="text-lg font-semibold text-center">{item.talk}</h2>
-                  <p className="text-gray-500 text-center">{item.time}</p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center">
+        {scheduleData.map((item, index) => (
+          <div key={index} className="bg-blue-200 border p-4 rounded text-center">
+            {editingIndex === index ? (
+              <div>
+                <button onClick={handleSaveEdit} className="bg-green-500 text-white p-2 rounded mr-2">
+                  Save
+                </button>
+              </div>
+            ) : (
+              <div>
+                <h2 className="text-lg font-semibold">{item.talk}</h2>
+                <p className="text-gray-500">{item.time}</p>
+                <div className="flex justify-center space-x-2 mt-2">
                   <button onClick={() => handleEdit(index)} className="bg-blue-500 text-white p-2 rounded">
                     Edit
                   </button>
@@ -123,13 +125,14 @@ function ConferenceSchedule() {
                     </button>
                   </Link>
                 </div>
-              )}
-            </div>
-          ))}
-        </div>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default ConferenceSchedule;
