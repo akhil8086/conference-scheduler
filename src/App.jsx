f327eb919d69810d18a103ed2a8ac5329c500f36
akhil8086 <akhiljith.r@edstem.com>
2023-11-17 14:47:44 +0530

    New layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,5 @@
 
 
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import LocationInfoPage from "./components/LocationInfoPage";
-// import Schedule from "./components/Schedule";
-// import Homepage from "./components/HomePage";
-// import Conference from "./components/Conferecnce";
-// import Speakers from "./components/Speakers";
-// import ConferenceDetails from "./components/ConferenceDetails";
-
-
-
-// function App() {
-//   return (
-//     <Router>
-//     <Routes>
-//     <Route path="/conference" element={<Conference />} />
-//       <Route path="/" element={<Homepage />} />
-//       <Route path="/location" element={<LocationInfoPage />} />
-//       <Route path="/speakers/:id/:ID" element={<Speakers/>}/>
-//       <Route path="/schedule/:id" element={<Schedule />} />
-//       <Route path="/details/:id" element={<ConferenceDetails />} />
-     
-//     </Routes>
-//   </Router>
-//   );
-// }
-
-// export default App;
-
-
 
 import HomePage from './components/HomePage'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
